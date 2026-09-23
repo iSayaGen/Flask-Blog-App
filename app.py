21ee5ed0c1c909c3eb0e24e052f1b1601ed0b66b
iSaya-X <illsaya@mail.de>
2026-09-23 12:47:47 +0200

    Add blog posts and index route

--- app.py
+++ app.py
@@ -1,11 +1,15 @@
-from flask import Flask
+from flask import Flask, render_template
+import json
 
 app = Flask(__name__)
 
 
 @app.route('/')
-def hello_world():
-    return 'Hello, World!'
+def index():
+    with open('blog.json', 'r') as file:
+        blog_posts = json.load(file)
+
+    return render_template('index.html', posts=blog_posts)
 
 
 if __name__ == '__main__':
