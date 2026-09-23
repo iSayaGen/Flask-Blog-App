c15afbd0495f6db74aafb65e1d1e6c0da5100bb0
iSaya-X <illsaya@mail.de>
2026-09-23 13:26:40 +0200

    Add create post functionality

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect, url_for
 import json
 
 app = Flask(__name__)
@@ -10,6 +10,40 @@ def index():
         blog_posts = json.load(file)
 
     return render_template('index.html', posts=blog_posts)
+
+
+@app.route('/add', methods=['GET', 'POST'])
+def add():
+    if request.method == 'POST':
+        author = request.form.get('author')
+        title = request.form.get('title')
+        content = request.form.get('content')
+
+        # Read the existing posts
+        with open('blog.json', 'r') as file:
+            blog_posts = json.load(file)
+
+        # Generate a new unique ID
+        new_id = max([post['id'] for post in blog_posts], default=0) + 1
+
+        # Create the new blog post
+        new_post = {
+            'id': new_id,
+            'author': author,
+            'title': title,
+            'content': content
+        }
+
+        # Add the new post to the list
+        blog_posts.append(new_post)
+
+        # Save the updated list back to blog.json
+        with open('blog.json', 'w') as file:
+            json.dump(blog_posts, file, indent=4)
+
+        return redirect(url_for('index'))
+
+    return render_template('add.html')
 
 
 if __name__ == '__main__':
